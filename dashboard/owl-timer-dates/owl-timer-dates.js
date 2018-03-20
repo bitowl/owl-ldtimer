46c16f5bd67f66546a233ceab5f6299a93e286cd
bitowl <git@bitowl.de>
2018-03-20 13:43:36 +0100

    Handle start and end times

--- dashboard/owl-timer-dates/owl-timer-dates.js
+++ dashboard/owl-timer-dates/owl-timer-dates.js
@@ -12,8 +12,12 @@
             super.ready();
             this.customText = 'initialText';
             timerConfig.on('change', newValue => {
+                console.log(newValue);
                 this.startDate = newValue.startDate;
                 this.endDate = newValue.endDate;
+                this.finishText = newValue.finishText;
+                this.startPrefix = newValue.startPrefix;
+                this.endPrefix = newValue.endPrefix;
             });
 
         }
@@ -21,6 +25,9 @@
         showMeThings() {
             timerConfig.value.startDate = this.$.startDate.value;
             timerConfig.value.endDate = this.$.endDate.value;
+            timerConfig.value.finishText = this.$.finishText.value;
+            timerConfig.value.startPrefix = this.$.startPrefix.value;
+            timerConfig.value.endPrefix = this.$.endPrefix.value;
         }
     }
     customElements.define(OwlTimerDates.is, OwlTimerDates);
--- extension/index.js
+++ extension/index.js
@@ -2,13 +2,16 @@
 
 module.exports = function (nodecg) {
     const ldTimer = nodecg.Replicant('timer', {defaultValue:{
-        text: "--:--:--",
+        text: '--:--:--',
     }, persistent: false});
 
     const timerConf = nodecg.Replicant('config', { 
         defaultValue: {
             startDate: '1323-11-23T15:21',
-            endDate: '1323-11-23T18:21'
+            endDate: '1323-11-23T18:21',
+            finishText: '--:--:--',
+            startPrefix: '',
+            endPrefix: ''
         }, persistent: true
     });
 
@@ -20,30 +23,54 @@ module.exports = function (nodecg) {
     var endDate = null;
 
     timerConf.on('change', newVal => {
-        startDate = new Date(newVal.startDate);
-        endDate = new Date(newVal.endDate);
-        console.log(startDate);
+        if (newVal.startDate === undefined) {
+            startDate = null;
+        } else {
+            startDate = new Date(newVal.startDate);
+        }
+        if (newVal.endDate === undefined) {
+            endDate = null;
+        } else {
+            endDate = new Date(newVal.endDate);
+        }
     });
 
     function tick() {
-        if (endDate === null) {
-            return;
+        var now = new Date();
+        if (startDate !== null) {
+            var timeUntilStart = startDate - now;
+            if (timeUntilStart >= 0) {
+                showTime(timerConf.value.startPrefix, timeUntilStart);
+                return;
+            }
         }
-        var t = endDate - new Date();
-        if (t < 0) {
-            ldTimer.value.text = "\\ö/";
-            return;
+
+        if (endDate !== null) {
+            var timeUntilEnd = endDate - now;
+            if (timeUntilEnd >= 0) {
+                showTime(timerConf.value.endPrefix, timeUntilEnd);
+                return;
+            }
         }
+
+        showFinishText();
+    }
+
+    function showTime(prefix, t) {
         var seconds = Math.floor( (t/1000) % 60 );
         var minutes = Math.floor( (t/1000/60) % 60 );
         var hours = Math.floor( (t/(1000*60*60)) );
 
-        ldTimer.value.text = doubleDigit(hours) + ":" + doubleDigit(minutes) + ":" + doubleDigit(seconds);
+        ldTimer.value.text = prefix + doubleDigit(hours) + ':' + doubleDigit(minutes) + ':' + doubleDigit(seconds);
+    }
+
+    function showFinishText() {
+        ldTimer.value.text = timerConf.value.finishText;
     }
 
     function doubleDigit(digit) {
         if (digit < 10) {
-            return "0" + digit;
+            return '0' + digit;
         }
         return digit;
     }
